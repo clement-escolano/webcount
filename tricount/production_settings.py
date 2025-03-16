1c26d8c1e4869145c4f682a1d69906fbfcd5ec23
Clément Escolano <clement.escolano@icloud.com>
2025-03-16 21:47:31 +0100

    Display error details in logs

--- tricount/production_settings.py
+++ tricount/production_settings.py
@@ -5,3 +5,21 @@ SECRET_KEY = environ["SECRET_KEY"]
 DEBUG = False
 
 ALLOWED_HOSTS = [environ["HOST"]]
+
+# Verbose logs for errors
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+    },
+}
